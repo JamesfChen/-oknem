57ae97af592818743d19d2a6a98ee097b8d3a973
jamesfchen <hawksjamesf@gmail.com>
2020-12-30 12:21:59 +0800

    add ip tcp protocol

--- androidvpn/app/src/main/java/com/jamesfchen/vpn/protocol/ip.kt
+++ androidvpn/app/src/main/java/com/jamesfchen/vpn/protocol/ip.kt
@@ -1,5 +1,9 @@
 package com.jamesfchen.vpn.protocol
 
+import android.nfc.Tag
+import android.util.Log
+import com.jamesfchen.vpn.*
+import java.net.InetAddress
 import java.nio.ByteBuffer
 
 /**
@@ -11,47 +15,214 @@ import java.nio.ByteBuffer
  * [IP报文格式](http://www.023wg.com/message/message/cd_feature_ip_message_format.html)
  * [IP协议头部](https://zhangbinalan.gitbooks.io/protocol/content/ipxie_yi_tou_bu.html)
  * [TCP 协议简介](http://www.ruanyifeng.com/blog/2017/06/tcp-protocol.html#:~:text=%E4%BB%A5%E5%A4%AA%E7%BD%91%E6%95%B0%E6%8D%AE%E5%8C%85%EF%BC%88packet,%E6%9C%80%E5%A4%9A%E4%B8%BA1480%E5%AD%97%E8%8A%82%E3%80%82)
+ * [rfc791](https://tools.ietf.org/html/rfc791)
+ *
  */
-
-val buffer: ByteBuffer = ByteBuffer.allocate(100)
+const val IP4_HEADER_SIZE = 20
+const val IP4_OPTION_HEADER_SIZE = 40
+const val IP4_HEADER_MAX_SIZE = 60
 
 //3bit
-enum class PrecedenceType(value: Int) {
-    Routine(0b000),//普通
-    Priority(0b001),//优先的
-    Immediate(0b010),//立即的发送
-    Flash(0b011),//闪电式的
-    Flash_Override(0b100),//比闪电还闪电式的
+enum class PrecedenceType(val typeValue: Int) {
+    /*
+            0     1     2     3     4     5     6     7
+      +-----+-----+-----+-----+-----+-----+-----+-----+
+      |                 |     |     |     |     |     |
+      |   PRECEDENCE    |  D  |  T  |  R  |  0  |  0  |
+      |                 |     |     |     |     |     |
+      +-----+-----+-----+-----+-----+-----+-----+-----+
+     */
+    UNKOWN(-1),
+    ROUTINE(0b000),//普通
+    PRIORITY(0b001),//优先的
+    IMMEDIATE(0b010),//立即的发送
+    FLASH(0b011),//闪电式的
+    FLASH_OVERRIDE(0b100),//比闪电还闪电式的
     CRITIC_ECP(0b101),//CRITIC/ECP
-    Internetwork_Control(0b110),//网间控制
-    Network_Control(0b111);//网络控制
+    INTERNETWORK_CONTROL(0b110),//网间控制
+    NETWORK_CONTROL(0b111);//网络控制
+
+    companion object {
+        fun parseType(v: Int): PrecedenceType {
+            for (e in values()) {
+                if (e.typeValue == v) {
+                    return e
+                }
+            }
+            return UNKOWN
+        }
+    }
 }
 
-//D 时延: 0:普通 1:延迟尽量小
-val normal_delay = 0
-val low_delay = 1
 
-//T 吞吐量: 0:普通 1:流量尽量大
-val normal_throughput = 0
-val high_throughput = 1
+data class TypeOfService(val byteValue: Int) {
+    companion object {
+        //D 时延: 0:普通 1:延迟尽量小
+        val normal_delay = 0
+        val low_delay = 1
 
-//R 可靠性: 0:普通 1:可靠性尽量大
-val normal_relibility = 0
-val high_relibility = 1
-class ServiceType{
-//    val PrecedenceType:PrecedenceType
-//    val d:Int
-//    val t:Int
-//    val r:Int
+        //T 吞吐量: 0:普通 1:流量尽量大
+        val normal_throughput = 0
+        val high_throughput = 1
+
+        //R 可靠性: 0:普通 1:可靠性尽量大
+        val normal_relibility = 0
+        val high_relibility = 1
+    }
+
+    val precedenceType: PrecedenceType = PrecedenceType.parseType(byteValue shr 5)
+    val d: Int = byteValue and 0b00010000
+    val t: Int = byteValue and 0b00001000
+    val r: Int = byteValue and 0b00000100
+    override fun toString(): String {
+        return "{\"precedenceType\":$precedenceType,\"d\":$d,\"t\":$t,\"r\":$r}"
+    }
 }
-class IpPacket(buffer: ByteBuffer) {
-    val version: Int//4bits
-    val ihl: Int//4bits
-    val typeOfService: Int//8bits
+
+data class IpFlag(val byteValue: Int) {
+    //    val reserved: Int //1bit
+    val df: Int = byteValue shr 1 and 0b01  //1bit Don't Fragment 能否分片位，0表示可以分片，1表示不能分片。
+    val mf: Int = byteValue  and 0b001 //1bit More Fragment 表示是否该报文为最后一片，0表示最后一片，1代表后面还有。
+    override fun toString(): String {
+        return "{\"df\":$df,\"mf\":$mf}"
+    }
+}
+
+enum class Protocol(val typeValue: Int) {
+    /*
+    常见值：
+    0: 保留Reserved
+    1: ICMP, Internet Control Message [RFC792]
+    2: IGMP, Internet Group Management [RFC1112]
+    3: GGP, Gateway-to-Gateway [RFC823]
+    4: IP in IP (encapsulation) [RFC2003]
+    6: TCP Transmission Control Protocol [RFC793]
+    17: UDP User Datagram Protocol [RFC768]
+    20: HMP Host Monitoring Protocol [RFC 869]
+    27: RDP Reliable Data Protocol [ RFC908 ]
+    46: RSVP (Reservation Protocol)
+    47: GRE (General Routing Encapsulation)
+    50: ESP Encap Security Payload [RFC2406]
+    51: AH (Authentication Header) [RFC2402]
+    54: NARP (NBMA Address Resolution Protocol) [RFC1735]
+    58: IPv6-ICMP (ICMP for IPv6) [RFC1883]
+    59: IPv6-NoNxt (No Next Header for IPv6) [RFC1883]
+    60: IPv6-Opts (Destination Options for IPv6) [RFC1883]
+    89: OSPF (OSPF Version 2) [RFC 1583]
+    112: VRRP (Virtual Router Redundancy Protocol) [RFC3768]
+    115: L2TP (Layer Two Tunneling Protocol)
+    124: ISIS over IPv4
+    126: CRTP (Combat Radio Transport Protocol)
+    127: CRUDP (Combat Radio User Protocol)
+    132: SCTP (Stream Control Transmission Protocol)
+    136: UDPLite [RFC 3828]
+    137: MPLS-in-IP [RFC 4023]
+     */
+    UNKOWN(-1),
+    Reserved(0),
+    ICMP(1),//IP数据包在发送途中一旦发生异常导致无法到达对端目标地址时，需要给发送端发送一个发生异常的通知。ICMP就是为这一功能而制定的。它有时也被用来诊断网络的健康状况。
+    IGMP(2),
+    IP(4),
+    TCP(6),
+    UDP(17),
+    IGRP(88),
+    OSPF(89),
+    MplsInIp(137);
+
+    companion object {
+        fun parseProtocol(v: Int): Protocol {
+            for (e in values()) {
+                if (e.typeValue == v) {
+                    return e
+                }
+            }
+            return UNKOWN
+        }
+    }
+
+}
+
+enum class IpVersion(val typeValue: Int) {
+    UNKOWN(-1),
+    V4(0b0100),
+    V6(0b0110);
+
+    companion object {
+        fun parseIpVersion(v: Int): IpVersion {
+            for (e in values()) {
+                if (e.typeValue == v) {
+                    return e
+                }
+            }
+            return UNKOWN
+        }
+    }
+}
+
+data class IpHeader(val buffer: ByteBuffer) {
+    /*
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |Version|  IHL  |Type of Service|          Total Length         |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |         Identification        |Flags|      Fragment Offset    |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |  Time to Live |    Protocol   |         Header Checksum       |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                       Source Address                          |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                    Destination Address                        |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                    Options                    |    Padding    |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        Example Internet Datagram Header
+     */
+    val version: IpVersion//4bits
+    val ihl: Int//4bits 描述IP包头的长度,由固定长度20字节+可变长度40字节，以4bytes为一个单位，固定长度20字节的值为20/4=5，需要用4bits表示5即为0101
+    val typeOfService: TypeOfService//8bits
+    val totalLength: Int//16bits 可存65535大小
+    val identification: Int//16bits 主机每发一个报文，加1，分片重组时会用到该字段。
+
+    val flags: IpFlag//3bits
+    val fragmentOffset: Int//13bits 分片重组时会用到该字段。表示较长的分组在分片后，某片在原分组中的相对位置。以8个字节为偏移单位
+    val ttl: Int//8bits Time to Live
+    val protocol: Protocol//8bits
+    val headerChecksum: Int//16bits
+    val sourceAddresses: InetAddress//32bits
+    val destinationAddresses: InetAddress //32bits
 
     init {
-        version = buffer[0].toInt() and 0xf0
-        ihl = buffer[0].toInt() and 0x0f
-        typeOfService = buffer[1].toInt()
+        val versionAndIhl = buffer.getUByte()
+        version = IpVersion.parseIpVersion(versionAndIhl shr 4)
+        ihl = (versionAndIhl and 0x0f).toInt() * 4
+        typeOfService = TypeOfService(buffer.getUByte())
+        totalLength = buffer.getUShort()
+        identification = buffer.getUShort()
+        val flagsAndFragment = buffer.getUShort()
+        flags = IpFlag(flagsAndFragment shr 13)
+        fragmentOffset = flagsAndFragment and 0b0001_1111_1111_1111
+        ttl = buffer.getUByte()
+        protocol = Protocol.parseProtocol(buffer.getUByte())
+        headerChecksum = buffer.getUShort()
+        sourceAddresses = InetAddress.getByAddress(buffer.getBytes(4))
+        destinationAddresses = InetAddress.getByAddress(buffer.getBytes(4))
+
+        val optionSize = ihl - IP4_HEADER_SIZE
+        if (optionSize > 0) {
+            //options
+
+        }
+
     }
+
+    override fun toString(): String {
+        return "\"ipheader\":{\"version\":$version,\"ihl\":$ihl,\"typeOfService\":$typeOfService,\"totalLength\":$totalLength," +
+                "\"identification\":$identification,\"flags\":$flags,\"fragmentOffset\":$fragmentOffset,\"ttl\":$ttl," +
+                "\"protocol\":$protocol,\"headerChecksum\":$headerChecksum," +
+                "\"sourceAddresses\":$sourceAddresses,\"destinationAddresses\":$destinationAddresses}"
+    }
+
+
 }
--- androidvpn/app/src/main/java/com/jamesfchen/vpn/protocol/tcp.kt
+++ androidvpn/app/src/main/java/com/jamesfchen/vpn/protocol/tcp.kt
@@ -2,12 +2,9 @@ package com.jamesfchen.vpn.protocol
 
 import android.os.*
 import android.util.Log
-import com.jamesfchen.vpn.Constants
-import java.io.Closeable
-import java.io.FileInputStream
-import java.io.FileOutputStream
+import com.jamesfchen.vpn.*
 import java.nio.ByteBuffer
-import java.nio.channels.FileChannel
+import kotlin.experimental.and
 
 /**
  * Copyright ® $ 2017
@@ -16,7 +13,10 @@ import java.nio.channels.FileChannel
  * @author: hawks.jamesf
  * @since: Dec/19/2020  Sat
  */
-const val T_TAG="${Constants.TAG}/tdp"
+const val T_TAG = "${Constants.TAG}/tdp"
+const val TCP_HEADER_SIZE = 20
+const val TCP_OPTION_HEADER_SIZE = 40
+
 class TcpHandlerThread() : HandlerThread("tcp_thread") {
 
 }
@@ -24,7 +24,103 @@ class TcpHandlerThread() : HandlerThread("tcp_thread") {
 class TcpHandler(looper: Looper) : Handler(looper) {
     override fun handleMessage(msg: Message) {
         super.handleMessage(msg)
-        Log.d(T_TAG,"tcp message")
+        Log.d(T_TAG, "tcp message")
+//        val packet = msg.obj as Packet
+//        val myClient = AioSocketClient()
+//        val destIp = packet.ipHeader.destinationAddresses.hostAddress
+//        val destPort = (packet.tlHeader as TcpHeader).destinationPort
+//        Log.d(T_TAG, "remote connect:${destIp}:${destPort}")
+//        myClient.connect(destIp, destPort)
+//        myClient.send(packet.buffer) { respBuffer ->
+//            Log.d(T_TAG, "buffer size:${respBuffer.remaining()}")
+//                                        pWriter.writePacket(Packet(header, respBuffer))
+//        }
     }
 
+}
+
+data class ControlBit(val byteValue: Int) {
+    /*
+    Control Bits:  6 bits (from left to right):
+
+    URG:  Urgent Pointer field significant
+    ACK:  Acknowledgment field significant
+    PSH:  Push Function
+    RST:  Reset the connection
+    SYN:  Synchronize sequence numbers
+    FIN:  No more data from sender
+     */
+    val hasURG: Boolean = ((byteValue and 0b100000) == 0b100000)
+    val hasACK: Boolean = ((byteValue and 0b010000) == 0b010000)
+    val hasPSH: Boolean = ((byteValue and 0b001000) == 0b001000)
+    val hasRST: Boolean = ((byteValue and 0b000100) == 0b000100)
+    val hasSYN: Boolean = ((byteValue and 0b000010) == 0b000010)
+    val hasFIN: Boolean = ((byteValue and 0b000001) == 0b000001)
+    override fun toString(): String {
+        return "ControlBit(hasURG=$hasURG, hasACK=$hasACK, hasPSH=$hasPSH, hasRST=$hasRST, hasSYN=$hasSYN, hasFIN=$hasFIN)"
+    }
+
+
+}
+
+data class TcpHeader(val buffer: ByteBuffer) : TransportLayerHeader {
+/*
+   0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |          Source Port          |       Destination Port        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                        Sequence Number                        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Acknowledgment Number                      |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |  Data |           |U|A|P|R|S|F|                               |
+   | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+   |       |           |G|K|H|T|N|N|                               |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |           Checksum            |         Urgent Pointer        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Options                    |    Padding    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                             data                              |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+
+    val sourcePort: Int
+    val destinationPort: Int
+    val sequenceNo: Long
+    val acknowledgmentNo: Long
+    val dataOffset: Int
+
+    //    val reserved: Int
+    val controlBit: ControlBit
+    val window: Int
+    val checksum: Int
+    val urgentPointer: Int
+    var optionsAndPadding: ByteArray? = null
+
+    init {
+        sourcePort = buffer.getUShort()
+        destinationPort = buffer.getUShort()
+        sequenceNo = buffer.getUInt()
+        acknowledgmentNo = buffer.getUInt()
+        val dataOffsetAndControlBitAndReserved = buffer.getUShort()
+        dataOffset = (dataOffsetAndControlBitAndReserved shr 12).toInt() * 4
+        controlBit = ControlBit(dataOffsetAndControlBitAndReserved and 0b0000000000_111111)
+        window = buffer.getUShort()
+        checksum = buffer.getUShort()
+        urgentPointer = buffer.getUShort()
+
+        val optionSize = dataOffset - TCP_HEADER_SIZE
+        if (optionSize > 0) {
+            //options
+            optionsAndPadding = ByteArray(optionSize)
+            buffer.get(optionsAndPadding, 0, optionSize)
+
+        }
+    }
+
+    override fun toString(): String {
+        return "TcpHeader(sourcePort=$sourcePort, destinationPort=$destinationPort, sequenceNo=$sequenceNo, acknowledgmentNo=$acknowledgmentNo, dataOffset=$dataOffset, controlBit=$controlBit, window=$window, checksum=$checksum, urgentPointer=$urgentPointer)"
+    }
 }
