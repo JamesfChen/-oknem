e63cea6421c6477bd6d5bdbe72ab43eb87226509
jamesfchen <hawksjamesf@gmail.com>
2020-12-30 12:23:03 +0800

    parse packet

--- androidvpn/app/src/main/java/com/jamesfchen/vpn/packet.kt
+++ androidvpn/app/src/main/java/com/jamesfchen/vpn/packet.kt
@@ -1,7 +1,10 @@
 package com.jamesfchen.vpn
 
+import android.system.Os
 import android.util.Log
+import com.jamesfchen.vpn.protocol.*
 import java.io.Closeable
+import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
 
@@ -11,37 +14,81 @@ import java.nio.channels.FileChannel
  *
  * @author: hawks.jamesf
  * @since: Dec/20/2020  Sun
+ *
+Protocol Layering
+
++---------------------+
+|     higher-level    |
++---------------------+
+|        TCP          |
++---------------------+
+|  internet protocol  |
++---------------------+
+|communication network|
++---------------------+
  */
-const val P_TAG="${Constants.TAG}/packet"
-enum class PacketType {
-    UNKNOWN,
-    UDP,
-    TCP
+
+const val P_TAG = "${Constants.TAG}/packet"
+const val BUFFER_SIZE = 16384//65535=2^15-1
+
+interface TransportLayerHeader
+data class Packet(val buffer: ByteBuffer) {
+    val ipHeader: IpHeader = IpHeader(buffer)
+    var tlHeader: TransportLayerHeader? = null
+    val payload: ByteBuffer
+
+    init {
+        when (ipHeader.protocol) {
+            Protocol.TCP -> {
+                tlHeader = TcpHeader(buffer)
+            }
+            Protocol.UDP -> {
+                tlHeader = UdpHeader(buffer)
+            }
+            else -> {
+            }
+        }
+        payload = buffer
+    }
+
+    override fun toString(): String {
+        return "Packet(ipHeader=$ipHeader, tlHeader=$tlHeader)"
+    }
+
 }
 
-const val IP4_HEADER_SIZE = 20
-const val TCP_HEADER_SIZE = 20
-const val UDP_HEADER_SIZE = 8
-const val BUFFER_SIZE= 16384
-
-data class Header(val type: PacketType)
-data class Packet(val header: Header, val buffer: ByteBuffer)
-
 class PacketReader(private val vpnInput: FileChannel) : AutoCloseable, Closeable {
+    val buffer = ByteBuffer.allocate(BUFFER_SIZE)
+
     init {
     }
 
     override fun close() {
+        Log.d(P_TAG, "packet reader closed")
         vpnInput.close()
     }
 
-    fun nextPacket(): Packet {
-        val header = Header(PacketType.TCP)
-        val buffer = ByteBuffer.allocate(BUFFER_SIZE)
-        vpnInput.read(buffer)
+    fun nextPacket(): Packet? {
+        var remaining = 0
+        try {
+            remaining = vpnInput.read(buffer)
+        } catch (e: IOException) {
+            Log.d(P_TAG, Log.getStackTraceString(e))
+        }
+        if (remaining == 0) {
+            return null
+        }
+        Log.d(
+            P_TAG,
+            "limit:${buffer.limit()} capacity:${buffer.capacity()}  remaining:${remaining}"
+        )
         buffer.flip()
-        Log.d(P_TAG,"read buffet${buffer}")
-        return Packet(header, buffer)
+        val packet = Packet(buffer)
+        Log.d(
+            P_TAG,
+            "read buffet: ${packet}"
+        )
+        return packet
     }
 
 }
@@ -56,7 +103,7 @@ class PacketWriter(private val vpnOutput: FileChannel) : AutoCloseable, Closeabl
         vpnOutput.close()
     }
 
-    fun writePacket(p:Packet) {
+    fun writePacket(p: Packet) {
     }
 
 }
